a43566150bdb4adbacf7c8011400936f64358777
kuishandaozhang <kuishandaozhang@example.com>
2019-12-22 14:32:36 +0800

    刚体的IsKinematic属性
    1，测试属性效果
    
    结论：
    1，勾选时物理效果消失；
    2，相当于禁用刚体组件；

---
+++ isKinematicOfRigidbody/Assets/Scripts/RigidbodyTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyTest : MonoBehaviour
+{
+    public Rigidbody m_Rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            m_Rigidbody.detectCollisions = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            m_Rigidbody.detectCollisions = true;
+        }
+    }
+}
